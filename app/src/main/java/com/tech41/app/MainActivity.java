c8344aeb55cf7528edf3bb69adef98850c21aa22
muditha41 <muditha41@gmail.com>
2020-09-01 00:49:25 +0530

    login and registration

---
+++ app/src/main/java/com/tech41/app/MainActivity.java
@@ -0,0 +1,33 @@
+package com.tech41.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tech41.app.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=(TextView)findViewById(R.id.mainlogo);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
